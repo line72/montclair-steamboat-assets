e014f214b8888951fe250749538f8724466570f2
Marcus Dillavou <line72@line72.net>
2019-12-27 06:24:09 -0700

    Updated to transmogrifier 0.9.3. Stop if any of the build steps fail

--- build.sh
+++ build.sh
@@ -4,10 +4,8 @@
 source scripts/VERSIONS
 
 # 1. Clone the repositories
-./scripts/clone-repos.sh
-
 # 2. Run the transmogrifier
-./scripts/run.sh
-
 # 3. Commit, tag, and push the changes
-./scripts/commit-and-tag-repos.sh
+./scripts/clone-repos.sh && \
+    ./scripts/run.sh && \
+    ./scripts/commit-and-tag-repos.sh
--- scripts/clone-repos.sh
+++ scripts/clone-repos.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 #
-
 set -e
 
 P=$(dirname $0)
