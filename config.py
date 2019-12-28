538f5b3beeea8a8818ec0b8038e5a1646248b418
Marcus Dillavou <line72@line72.net>
2019-12-28 07:30:50 -0700

    Bumped the revision of everything

--- config.py
+++ config.py
@@ -12,21 +12,21 @@ CONFIG = transmogrifier.Config(
     logo_svg = 'assets/logo.svg',
     montclair_config = transmogrifier.MontclairConfig(
         version = '1.2.1',
-        revision = '6',
+        revision = '7',
         title = 'Go Steamboat',
         first_run_text = "Welcome to Steamboat Springs, CO's Real Time Bus Tracker.<br /><br />Please select one or more routes to begin!",
         configuration_js_file = 'assets/Configuration.js'
     ),
     ios_config = transmogrifier.MontclairiOSConfig(
         version = '2.0.1',
-        revision = '1',
+        revision = '2',
         app_id = 'com.gotransitapp.steamboat',
         app_store_id = 'REPLACE_ME',
         app_store_url = ''
     ),
     android_config = transmogrifier.MontclairAndroidConfig(
         version = '1.0.1',
-        revision = '6',
+        revision = '7',
         app_id = 'com.gotransitapp.steamboat',
         play_store_url = 'https://play.google.com/store/apps/details?id=com.gotransitapp.steamboat'
     )
